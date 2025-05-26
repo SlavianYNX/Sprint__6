5df9f929e581219860f395a947419088e278e3ee
Slava Eremin <slavik.eremin@yandex.ru>
2025-05-26 10:53:46 +0300

    add сconcatenation

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 class Urls:
     DZEN_URL = "https://dzen.ru/?yredirect=true"
     QA_SCOOTER_URL = "https://qa-scooter.praktikum-services.ru/"
-    QA_SCOOTER_ORDER_URL = "https://qa-scooter.praktikum-services.ru/order"
-    QA_SCOOTER_STATUS_URL = "https://qa-scooter.praktikum-services.ru/track"
+    QA_SCOOTER_ORDER_IN = "order"
+    QA_SCOOTER_STATUS_IN = "track"
+    QA_SCOOTER_ORDER_URL = QA_SCOOTER_URL + QA_SCOOTER_ORDER_IN
+    QA_SCOOTER_STATUS_URL = QA_SCOOTER_URL + QA_SCOOTER_STATUS_IN
